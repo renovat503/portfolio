ac3ebb94f945d5573af4e3601f7453f4df25ae04
renovat503 <85083316+renovat503@users.noreply.github.com>
2021-09-12 06:02:58 -0600

    commit three

--- src/components/contentarea.jsx
+++ src/components/contentarea.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import About from '../components/about';
-import Experience from '../components/experience';
 import Work from '../components/work';
-import Contact from '../components/contact';
 class ContentArea extends Component {
     state = {}
     render() {
         return (
             <main className="container">
+                <Switch>
+                <Route path="/" exact component={About}></Route>
                 <Route path="/about" component={About}></Route>
-                <Route path="/experience" component={Experience}></Route>
                 <Route path="/work" component={Work}></Route>
-                <Route path="/contact" component={Contact}></Route>
+                </Switch>
             </main>
         );
     }
