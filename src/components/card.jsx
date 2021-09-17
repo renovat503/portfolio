94f6c76172af44bf2a5bc6ab47a5b80b6346e6c6
renovat503 <85083316+renovat503@users.noreply.github.com>
2021-09-17 07:06:42 -0600

    added dribbble link

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../styles/card.css';
 import { VscGithub } from "react-icons/vsc";
 import { VscLinkExternal } from "react-icons/vsc";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDribbble} from '@fortawesome/free-brands-svg-icons';
 class Card extends React.Component {
     render() { 
         return <React.Fragment>
@@ -9,6 +11,7 @@ class Card extends React.Component {
                         <div className = "card">
                             <div className="icons">
                                 <span className="card-icon"><a target="_blank" href={this.props.link}><VscLinkExternal /></a></span>
+                                <span className="card-icon"><a target="_blank" href={this.props.linkThree}><FontAwesomeIcon icon={faDribbble} ></FontAwesomeIcon></a></span>
                                 <span className="card-icon"><a target="_blank" href={this.props.linkTwo}><VscGithub /></a></span>
                             </div>
 
