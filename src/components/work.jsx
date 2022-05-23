7cf7f17c90f2ff21b85fdcaa54a1a287d02588fe
renova <85083316+renovat503@users.noreply.github.com>
2022-05-22 22:39:17 -0600

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -8,12 +8,19 @@ class Work extends Component {
         return (
             <React.Fragment>
                 <div className="main-container">
-                    <a target="_blank"href="https://message01.netlify.app/"><Card textOne="Message" 
-                        textTwo="Application" 
-                        stacks="HTML/CSS  JS" 
-                        linkTwo="https://github.com/renovat503/message-application" 
+                    <a target="_blank"href="https://shoeboxx.netlify.app/"><Card textOne="Shoebox" 
+                        textTwo="Marketplace" 
+                        stacks="Chakra UI  JS" 
+                        linkTwo="https://github.com/renovat503/shoebox" 
                         linkThree = "https://dribbble.com/Renny99"
-                        link = "https://message01.netlify.app/"
+                        link = "https://shoeboxx.netlify.app/"
+                        /></a>
+                     <a target="_blank"href="https://meetup99.netlify.app/"><Card textOne="Meetup" 
+                        textTwo="Clone" 
+                        stacks="HTML/CSS  JS" 
+                        linkTwo="https://github.com/renovat503/netflix_clone" 
+                        linkThree = "https://dribbble.com/Renny99"
+                        link = "https://netflix99.netlify.app/"
                         /></a>
 
                     <a target="_blank"href="https://umbrella99.netlify.app/"><Card textOne="Weather" 
@@ -31,20 +38,12 @@ class Work extends Component {
                         linkThree = "https://dribbble.com/Renny99"
                         link = "https://netflix99.netlify.app/"
                         /></a>
-            
-                    <a target="_blank"href="https://meetup99.netlify.app/"><Card textOne="Meetup" 
-                        textTwo="Clone" 
+                     <a target="_blank"href="https://message01.netlify.app/"><Card textOne="Message" 
+                        textTwo="Application" 
                         stacks="HTML/CSS  JS" 
-                        linkTwo="https://github.com/renovat503/netflix_clone" 
+                        linkTwo="https://github.com/renovat503/message-application" 
                         linkThree = "https://dribbble.com/Renny99"
-                        link = "https://netflix99.netlify.app/"
-                        /></a>
-                    <a target="_blank"href="https://shoeboxx.netlify.app/"><Card textOne="Shoebox" 
-                        textTwo="Marketplace" 
-                        stacks="Chakra UI  JS" 
-                        linkTwo="https://github.com/renovat503/shoebox" 
-                        linkThree = "https://dribbble.com/Renny99"
-                        link = "https://shoeboxx.netlify.app/"
+                        link = "https://message01.netlify.app/"
                         /></a>
                     <Card textOne="coming" textTwo="soon" stacks="HTML/CSS  JS PHP"/>
                     <DesignCard textOne="coming" textTwo="soon" stacks="PS FIGMA"/>
