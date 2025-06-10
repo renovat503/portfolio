8f5da1666da4fdf5725e6d73d502d174d97a1264
renova <85083316+renovat503@users.noreply.github.com>
2025-06-10 05:00:22 -0600

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -8,6 +8,13 @@ class Work extends Component {
         return (
             <React.Fragment>
                 <div className="main-container">
+                    <a target="_blank"href="https://payinapo.com"><Card textOne="Payinapo" 
+                        textTwo="Web app" 
+                        stacks="RN/.Net Core JS" 
+                        linkTwo="https://github.com/renovat503/" 
+                        linkThree = "https://dribbble.com/Renny99"
+                        link = "https://payinapo.com"
+                        /></a>
                     <a target="_blank"href="https://play.google.com/store/apps/details?id=com.renova.payinapo"><Card textOne="Payinapo" 
                         textTwo="Mobile app" 
                         stacks="RN/.Net Core JS" 
